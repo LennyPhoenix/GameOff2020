4457421ad7ff023ec95fa0fff078e1968a778ce5
Lenny <LennyPhoenixC@gmail.com>
2020-11-04 21:01:11 +0000

    Added new player sprites, animations and state machine.

--- Source/Entities/Player.cs
+++ Source/Entities/Player.cs
@@ -3,6 +3,15 @@ using System;
 
 public class Player : KinematicBody2D
 {
+    // State Machine
+    public enum State
+    {
+        Idle,
+        Move,
+        Turn,
+    }
+    public State state;
+
     // Movement
     [Export]
     public float acceleration = 500f;
@@ -11,7 +20,7 @@ public class Player : KinematicBody2D
     [Export]
     public float friction = 800f;
     [Export]
-    public float cameraForwardMult = 30f;
+    public float cameraForwardMult = 40f;
 
     public float drag;
     public Vector2 velocity = Vector2.Zero;
@@ -21,7 +30,7 @@ public class Player : KinematicBody2D
     [Export]
     public float rotationSpeed = 5f;
     [Export]
-    public float rotationTimerStart = 0.1f;
+    public float rotationTimerStart = 0.3f;
 
     public float rotationStart = 0f;
     public float rotationEnd = 0f;
@@ -29,21 +38,26 @@ public class Player : KinematicBody2D
 
     public Node2D rotate;
     public Camera2D camera;
+    public AnimationPlayer animationPlayer;
 
     public override void _Ready()
     {
         base._Ready();
 
+        state = State.Idle;
+
         rotationTimer = rotationTimerStart;
         
         rotate = GetNode<Node2D>("Rotate");
         camera = GetNode<Camera2D>("Camera");
+        animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
     }
 
     public override void _Process(float delta)
     {
         base._Process(delta);
 
+        // Get Input
         inputVec = Vector2.Zero;
         inputVec.x += Input.GetActionStrength("right");
         inputVec.x -= Input.GetActionStrength("left");
@@ -51,43 +65,101 @@ public class Player : KinematicBody2D
         inputVec.y -= Input.GetActionStrength("up");
         inputVec = inputVec.Normalized();
 
-        camera.Position = inputVec * cameraForwardMult;
-
-        // Get target rotation
+        // Rotate To Mouse
         float target = GetGlobalMousePosition().AngleToPoint(rotate.GlobalPosition);
 
-        // Reset lerp if target is different.
         if (target != rotationEnd)
         {
-            rotationStart = rotate.Rotation;
+            rotationStart = rotate.GlobalRotation;
             rotationEnd = target;
             rotationTimer = rotationTimerStart;
         }
 
-        // Reset lerp if angle is reached, else lerp.
-        if (rotate.Rotation != rotationEnd)
+        if (rotate.GlobalRotation != rotationEnd)
         {
-            rotate.Rotation = Mathf.LerpAngle(rotate.Rotation, rotationEnd, rotationTimer);
+            rotate.GlobalRotation = Mathf.LerpAngle(rotate.GlobalRotation, rotationEnd, rotationTimer);
 
             rotationTimer += delta * rotationSpeed;
 
             rotationTimer = Math.Min(rotationTimer, 1f);
         }
+
+        switch (state)
+        {
+            case State.Idle:
+                animationPlayer.Play("Idle");
+
+                if (inputVec != Vector2.Zero)
+                {
+                    state = State.Move;
+                }
+
+                if (Mathf.Round(rotate.GlobalRotation * 2) / 2 != Mathf.Round(rotationEnd * 2) / 2)
+                {
+                    state = State.Turn;
+                }
+
+                break;
+
+            case State.Move:
+                animationPlayer.Play("Move");
+
+                // Offset Camera
+                camera.Position = inputVec * cameraForwardMult;
+
+                if (inputVec == Vector2.Zero)
+                {
+                    state = State.Idle;
+                }
+
+                break;
+
+            case State.Turn:
+                animationPlayer.Play("Move");
+
+                if (Mathf.Round(rotate.GlobalRotation * 2) / 2 == Mathf.Round(rotationEnd * 2) / 2)
+                {
+                    state = State.Idle;
+                }
+
+                if (inputVec != Vector2.Zero)
+                {
+                    state = State.Move;
+                }
+
+                GD.Print(Mathf.Round(rotate.GlobalRotation * 2) / 2, Mathf.Round(rotationEnd * 2) / 2);
+
+                break;
+        }
+
+        GD.Print(state);
     }
 
     public override void _PhysicsProcess(float delta)
     {
         base._PhysicsProcess(delta);
 
-        if (inputVec != Vector2.Zero)
+        switch (state)
         {
-            velocity = velocity.MoveToward(inputVec * maxSpeed, acceleration * delta);
-        }
-        else
-        {
-            velocity = velocity.MoveToward(Vector2.Zero, friction * delta);
-        }
+            case State.Idle:
+                break;
 
-        velocity = MoveAndSlide(velocity);
+            case State.Move:
+                if (inputVec != Vector2.Zero)
+                {
+                    velocity = velocity.MoveToward(inputVec * maxSpeed, acceleration * delta);
+                }
+                else
+                {
+                    velocity = velocity.MoveToward(Vector2.Zero, friction * delta);
+                }
+
+                velocity = MoveAndSlide(velocity);
+
+                break;
+
+            case State.Turn:
+                break;
+        }
     }
 }
