cbd4de02632c4c21d6706102e95afde20840eafb
Lenny <LennyPhoenixC@gmail.com>
2020-11-22 14:59:32 +0000

    🎯 Add mining display to drills.
    TODO: Rebalance drills.

--- Source/Buildings/Drills/Drill.cs
+++ Source/Buildings/Drills/Drill.cs
@@ -8,16 +8,46 @@ public class Drill : Building
 
     public AnimationPlayer SpriteAnimationPlayer;
     public TileMap OreTiles;
-    public Array<Ore> Ores;
+    public Dictionary<Ore, int> Ores;
+
+    public PanelContainer MiningContainer;
+    public GridContainer MiningGridContainer;
+    public Dictionary<Ore, StorageItem> MiningItems = new Dictionary<Ore, StorageItem>();
 
     public override void _Ready()
     {
+        MiningContainer = GetNode<PanelContainer>("UI/MiningContainer");
+        MiningGridContainer = GetNode<GridContainer>("UI/MiningContainer/GridContainer");
+
         base._Ready();
 
         SpriteAnimationPlayer = GetNode<AnimationPlayer>("Sprite/AnimationPlayer");
         OreTiles = GetTree().CurrentScene.GetNode<TileMap>("Planet/Ore");
 
         Ores = GetOres();
+
+        if (Ores.Count == 0)
+        {
+            WarningSprite.Visible = true;
+            MiningContainer.Visible = false;
+        }
+
+        int columns = Mathf.Min(4, Ores.Count);
+        int rows = Mathf.CeilToInt((float)Ores.Count / 4);
+        MiningContainer.RectSize = new Vector2(columns * 16 + 2 + Mathf.Min(columns - 1, 0), rows * 16 + 2 + Mathf.Min(rows - 1, 0));
+        MiningGridContainer.Columns = columns;
+
+        foreach (Ore ore in Ores.Keys)
+        {
+            var miningItem = (StorageItem)StorageItemScene.Instance();
+            MiningGridContainer.AddChild(miningItem);
+            miningItem.ItemType = (Item)ore;
+            miningItem.Count = Ores[ore] * MiningAmount;
+
+            MiningItems.Add(ore, miningItem);
+        }
+
+        Globals.LastBuilding = this;
     }
 
     public override void Tick()
@@ -29,32 +59,39 @@ public class Drill : Building
 
         base.Tick();
 
-        foreach (Ore ore in Ores)
+        foreach (Ore ore in Ores.Keys)
         {
             var item = (Item)ore;
 
-            Items[item] += MiningAmount;
+            Items[item] += MiningAmount * Ores[ore];
             Items[item] = Mathf.Min(Items[item], MaxStorage[item]);
         }
     }
 
-    private Array<Ore> GetOres()
+    private Dictionary<Ore, int> GetOres()
     {
         Vector2 offset = new Vector2(Mathf.FloorToInt(Size / 2), Mathf.FloorToInt(Size / 2));
         Vector2 topLeft = (GlobalPosition / 16) - offset;
 
-        var ores = new Array<Ore>();
+        var ores = new Dictionary<Ore, int>();
 
         for (int x = 0; x < Size; x++)
         {
             for (int y = 0; y < Size; y++)
             {
-                int ore = OreTiles.GetCell(x + Mathf.FloorToInt(topLeft.x), y + Mathf.FloorToInt(topLeft.y));
-                var item = (Item)ore;
+                int oreId = OreTiles.GetCell(x + Mathf.FloorToInt(topLeft.x), y + Mathf.FloorToInt(topLeft.y));
+                var ore = (Ore)oreId;
 
-                if (ore != -1 && MaxStorage.ContainsKey(item))
+                if (oreId != -1 && MaxStorage.ContainsKey((Item)ore))
                 {
-                    ores.Add((Ore)ore);
+                    if (!ores.ContainsKey(ore))
+                    {
+                        ores.Add(ore, 1);
+                    }
+                    else
+                    {
+                        ores[ore] += 1;
+                    }
                 }
             }
         }
